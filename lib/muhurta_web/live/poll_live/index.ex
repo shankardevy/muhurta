3425064bc5ea6da8c495344fbe82f26a9a9050e0
Shankar Dhanasekaran <shankardevy@gmail.com>
2024-08-22 04:17:28 +0530

    Remove unrelated comments

--- lib/muhurta_web/live/poll_live/index.ex
+++ lib/muhurta_web/live/poll_live/index.ex
@@ -7,18 +7,6 @@ defmodule MuhurtaWeb.PollLive.Index do
     polls = Events.list_polls!()
     user = Muhurta.Events.get_user!(user_id)
 
-    # In Elixir, keyword lists are defined with a square brackets like this:
-    #
-    # [name: "chaaru", favorite_animal: "all animals"]`
-    #
-    # However, when passing this list as a last argument to a function, it can be
-    # written without brackets like: some_func_call(first_arg, name: "chaaru", favorite_animal: "all animals")
-    #
-    # which is same as: some_func_call(first_arg, [name: "chaaru", favorite_animal: "all animals"])
-    #
-    # This is the reason why assign(socket, polls: polls, current_user: user) looks like
-    # it's taking more arguments than it actually is. When in fact the second argument is just a single keyword list.
-
     {:ok, assign(socket, polls: polls, current_user: user)}
   end
 end
