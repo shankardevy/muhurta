b5178470145d62f860fcf741a2c9f48c5423166d
Shankar Dhanasekaran <shankardevy@gmail.com>
2024-08-22 02:55:51 +0530

    Use assign/2 to set more than one variable at a time

--- lib/muhurta_web/live/poll_live/index.ex
+++ lib/muhurta_web/live/poll_live/index.ex
@@ -3,23 +3,22 @@ defmodule MuhurtaWeb.PollLive.Index do
 
   alias Muhurta.Events
 
-  # `conn` and `socket` are two distinct states: conn for http and socket for websocket
-  #
-  # When a LiveView is mounted, it doesn't have access to the previous conn.assigns used in the http connection.
-  # It can only access what is set in the session via `put_session`
-  #
-  # Due to this even though `current_user` is set in conn.assigns during the initial pipeline request in `router.ex`,
-  # LiveView doesn't have access to this state once it switches over to WebSocket communication.
-  # Therefore, it is necessary to load the current_user again from the session to maintain state
-  # and ensure the LiveView can function independently of the conn.
-
   def mount(_params, %{"user_id" => user_id} = _session, socket) do
     polls = Events.list_polls!()
     user = Muhurta.Events.get_user!(user_id)
 
-    {:ok,
-     socket
-     |> assign(:polls, polls)
-     |> assign(:current_user, user)}
+    # In Elixir, keyword lists are defined with a square brackets like this:
+    #
+    # [name: "chaaru", favorite_animal: "all animals"]`
+    #
+    # However, when passing this list as a last argument to a function, it can be
+    # written without brackets like: some_func_call(first_arg, name: "chaaru", favorite_animal: "all animals")
+    #
+    # which is same as: some_func_call(first_arg, [name: "chaaru", favorite_animal: "all animals"])
+    #
+    # This is the reason why assign(socket, polls: polls, current_user: user) looks like
+    # it's taking more arguments than it actually is. When in fact the second argument is just a single keyword list.
+
+    {:ok, assign(socket, polls: polls, current_user: user)}
   end
 end
