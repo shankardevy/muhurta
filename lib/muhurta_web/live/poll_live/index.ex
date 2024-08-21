b588bce8020196ff69bf84b7f23688b67c45bcf6
Shankar Dhanasekaran <shankardevy@gmail.com>
2024-08-22 04:18:25 +0530

    Create a simple function component

--- lib/muhurta_web/live/poll_live/index.ex
+++ lib/muhurta_web/live/poll_live/index.ex
@@ -3,6 +3,12 @@ defmodule MuhurtaWeb.PollLive.Index do
 
   alias Muhurta.Events
 
+  # Importing `MuhurtaWeb.EventComponents` in this LiveView module allows us
+  # to use the function components defined in `MuhurtaWeb.EventComponents` in our
+  # template files in the shorthand format like `<.poll_card />` instead of the long
+  # format of using `<MuhurtaWeb.EventComponents.poll_card />`.
+  import MuhurtaWeb.EventComponents
+
   def mount(_params, %{"user_id" => user_id} = _session, socket) do
     polls = Events.list_polls!()
     user = Muhurta.Events.get_user!(user_id)
