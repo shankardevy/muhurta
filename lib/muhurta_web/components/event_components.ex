fb89034f69a813b66b13d38da766904c6f8b9dc5
Shankar Dhanasekaran <shankardevy@gmail.com>
2024-08-22 06:12:47 +0530

    Create multiple slots

--- lib/muhurta_web/components/event_components.ex
+++ lib/muhurta_web/components/event_components.ex
@@ -7,18 +7,19 @@ defmodule MuhurtaWeb.EventComponents do
 
   attr :poll, :map, required: true
   attr :rest, :global, default: %{class: "px-8 py-4 bg-white rounded-lg shadow-md"}
+
   slot :inner_block
+  slot :name
+  slot :location
 
   def poll_card(assigns) do
     ~H"""
     <div {@rest}>
       <div class="flex items-center justify-between">
-        <span class="text-sm font-light text-gray-600"><%= @poll.location %></span>
+        <%= render_slot(@location) %>
       </div>
       <div class="mt-2">
-        <a href="#" class="text-xl font-bold text-gray-700 hover:text-gray-600 hover:underline">
-          <%= @poll.name %>
-        </a>
+        <%= render_slot(@name) %>
 
         <%= render_slot(@inner_block) %>
       </div>
