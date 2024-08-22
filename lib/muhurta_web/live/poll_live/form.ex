77ca3016bbb360b783e0629810fe06bb1414a165
Shankar Dhanasekaran <shankardevy@gmail.com>
2024-08-22 23:05:59 +0530

    Add a simple brittle form without changeset

--- lib/muhurta_web/live/poll_live/form.ex
+++ lib/muhurta_web/live/poll_live/form.ex
@@ -6,6 +6,17 @@ defmodule MuhurtaWeb.PollLive.Form do
   def mount(_params, %{"user_id" => user_id} = _session, socket) do
     user = Events.get_user!(user_id)
 
-    {:ok, assign(socket, current_user: user)}
+    poll_form = %{
+      name: "",
+      location: "",
+      description: ""
+    }
+
+    {:ok, assign(socket, form: to_form(poll_form), current_user: user)}
+  end
+
+  def handle_event("submit", params, socket) do
+    Events.create_poll!(params, actor: socket.assigns.current_user)
+    {:noreply, put_flash(socket, :info, "Event saved")}
   end
 end
