6eb2a83d7466cb546bf132b5984fc2ae810c2f90
Shankar Dhanasekaran <shankardevy@gmail.com>
2024-08-22 23:28:17 +0530

    Use Ash Form Helper to render functional form

--- lib/muhurta_web/live/poll_live/form.ex
+++ lib/muhurta_web/live/poll_live/form.ex
@@ -6,17 +6,31 @@ defmodule MuhurtaWeb.PollLive.Form do
   def mount(_params, %{"user_id" => user_id} = _session, socket) do
     user = Events.get_user!(user_id)
 
-    poll_form = %{
-      name: "",
-      location: "",
-      description: ""
-    }
-
-    {:ok, assign(socket, form: to_form(poll_form), current_user: user)}
+    {:ok, assign(socket, current_user: user)}
   end
 
-  def handle_event("submit", params, socket) do
-    Events.create_poll!(params, actor: socket.assigns.current_user)
-    {:noreply, put_flash(socket, :info, "Event saved")}
+  def handle_params(params, _uri, socket) do
+    {:noreply, assign(socket, form: get_poll_form(params, socket.assigns.current_user))}
+  end
+
+  def handle_event("submit", %{"form" => params}, socket) do
+    case AshPhoenix.Form.submit(socket.assigns.form,
+           params: params
+         ) do
+      {:ok, poll} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Event Poll created")
+         |> push_navigate(to: ~p"/polls/#{poll.id}")}
+
+      {:error, form} ->
+        {:noreply, assign(socket, :form, form)}
+    end
+  end
+
+  defp get_poll_form(_params, user) do
+    Muhurta.Events.Poll
+    |> AshPhoenix.Form.for_create(:create, actor: user)
+    |> to_form()
   end
 end
